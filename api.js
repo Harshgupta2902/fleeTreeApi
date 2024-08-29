826e1ce9fa75a5c5b447c664d899bcf05808728d
Harsh <harsh.gupta@cognustechnology.com>
2024-08-29 11:49:26 +0530

    chnages

--- api.js
+++ api.js
@@ -6,7 +6,6 @@ const app = express();
 // Define API endpoints
 app.use("/details", details);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
