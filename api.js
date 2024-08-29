fecf84205b318c3fecdb320487efe6355e7bb150
Harsh <harsh.gupta@cognustechnology.com>
2024-08-29 12:04:26 +0530

    minor changes

--- api.js
+++ api.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const details = require("./details");
+const insert = require("./insert");
 
 const app = express();
 
 // Define API endpoints
 app.use("/details", details);
+app.use("/insert", insert);
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${3000}`);
